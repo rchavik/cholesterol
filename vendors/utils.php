549ad445839370bc7a307b5f79d5c17a8b85dfca
Rachman Chavik <rchavik@gmail.com>
2010-08-16 10:19:43 +0700

    removing unused function object_to_array()

--- vendors/utils.php
+++ vendors/utils.php
@@ -1,31 +1,6 @@
 <?php
 // vim: set ts=4 sts=4 sw=4 si noet:
 
-if (!function_exists('object_to_array')):
-
-function object_to_array($var) {
-	$result = array();
-	$references = array();
-
-	// loop over elements/properties
-	foreach ($var as $key => $value) {
-		// recursively convert objects
-		if (is_object($value) || is_array($value)) {
-			// but prevent cycles
-			if (!in_array($value, $references)) {
-				$result[$key] = object_to_array($value);
-				$references[] = $value;
-			}
-		} else {
-			// simple values are untouched
-			$result[$key] = $value;
-		}
-	}
-	return $result;
-}
-
-endif;
-
 function array_key_value($key, $array, $emptyValue = 0) {
 	return array_key_exists($key, $array) ? $array[$key] : $emptyValue;
 }
